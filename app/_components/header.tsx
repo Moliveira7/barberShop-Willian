310552e5ed96c868de5f2f087707ace5db0448ac
Moliveira7 <m7oliveiraf@gmail.com>
2024-04-30 18:49:15 -0300

    feat: add AuthProvider

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,16 +1,18 @@
+"use client";
+
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
 
-const Header = () => {
+const Header = () => { 
   return (
     <Card>
       <CardContent className="p-5 justify-between items-center flex flex-row">
         <Image src="/logo.png" alt="FSW Barber" height={22} width={120} />
         <Button variant="outline" size="icon" className="h-8 w-8">
           <MenuIcon size={16} />
-        </Button>
+        </Button>        
       </CardContent>
     </Card>
   );
